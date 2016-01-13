34e04f2867946e63b85a3f6a258ba70ff45c6911
JoeyFTribbiani <vincent@VincentdeMacBook-Pro.local>
2016-01-13 10:41:58 -0500

    0.0.1

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,16 @@
 require("cloud/app.js");
+
+var studySessionOption = AV.Object.extend("StudySessionOption")
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
-AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+AV.Cloud.define("/votes", function(request, response) {
+  var query = new AV.Query(studySessionOption)
+  query.find({
+    success:function(options){
+      res.render('/votes/votes',{
+        title:"select your session time",
+        options:options
+      })
+    }
+  })
 });
