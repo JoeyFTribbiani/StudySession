c6429700e575f8f0eb7b2ecfdc6dce918c84c4ea
JoeyFTribbiani <vincent@VincentdeMacBook-Pro.local>
2016-01-13 11:05:21 -0500

    0.0.3

--- cloud/app.js
+++ cloud/app.js
@@ -1,22 +1,23 @@
 // 在 Cloud code 里初始化 Express 框架
 var express = require('express');
 var app = express();
+var StudySessionOption = AV.Object.extend("StudySessionOption")
 
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
-var studySessionOption = AV.Object.extend("StudySessionOption")
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
-app.get("/votes", function(request, response) {
-  var query = new AV.Query(studySessionOption)
+
+app.get("/votes", function(req, res) {
+  var query = new AV.Query(StudySessionOption)
   query.find({
     success:function(options){
-      res.render('/votes/votes',{
+      res.render('votes/votes',{
         title:"select your session time",
         options:options
       })
