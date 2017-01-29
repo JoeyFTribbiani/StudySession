f8042b4c35d17ef3d14b5ac35bbfa9ba8c2bedde
JoeyFTribbiani <vincent@VincentdeMacBook-Pro.local>
2017-01-29 16:51:06 -0500

    update

--- cloud/app.js
+++ cloud/app.js
@@ -16,7 +16,7 @@ app.get('/hello', function(req, res) {
 });
 
 app.get("/index", function(req, res) {
-  res.render('mockup/index')
+  res.render('mockup/index.html')
 });
 
 app.get("/votes", function(req, res) {
